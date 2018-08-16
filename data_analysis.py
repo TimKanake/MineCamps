bc3b3e37412fa225177cacb27f95288549f4b94d
Timothy Mwiti <mwititimothyk@gmail.com>
2018-08-16 10:53:21 -0500

    Emotion analysis functions

---
+++ data_analysis.py
@@ -0,0 +1,52 @@
+import csv
+import sys
+import collections
+import matplotlib.pyplot as plt
+import math
+
+
+def analyze_emotion(csv_file_path, emotion):
+    emotion_levels = get_emotion_levels(csv_file_path, emotion)
+
+    total_emotion_value = 0
+    max_emotion_value = sys.float_info.min
+    # areas_of_interest = {}
+    for key, value in emotion_levels.items():
+        total_emotion_value += value
+        if value > max_emotion_value:
+            max_emotion_value = value
+
+    average_emotion_value = float(total_emotion_value/len(emotion_levels))
+    print('average emotion value: ', average_emotion_value)
+    print('max emotion value: ', max_emotion_value)
+
+    dist_from_mean_sum = 0
+    for key, value in emotion_levels.items():
+        dist_from_mean_sum += (value - average_emotion_value)**2
+    std_deviation = math.sqrt(dist_from_mean_sum/(len(emotion_levels)-1))
+
+    print('standard deviation: ', std_deviation)
+
+
+def convert_delimited_txt_to_csv(txt_file_path, csv_file_path):
+    in_txt = csv.reader(open(txt_file_path, "rb"), delimiter='\t')
+    out_csv = csv.writer(open(csv_file_path, 'wb'))
+    out_csv.writerows(in_txt)
+
+
+def get_emotion_levels(file_path, emotion):
+    with open(file_path) as csvfile:
+        data = csv.DictReader(csvfile)
+        values = {}
+        vals = []
+        for row in data:
+            if int(row['NoOfFaces']) == 1 and \
+             float(row[emotion + ' Evidence']) > 0:
+                values[row['Timestamp']] = float(row[emotion + ' Evidence'])
+                # vals.append(float(row[emotion + ' Evidence']))
+    return values
+
+
+if __name__ == '__main__':
+    analyze_emotion(
+        '/Users/timkanake/Desktop/imotionsdata.csv', 'Confusion')
--- training.py
+++ training.py
@@ -3,14 +3,21 @@ from os.path import isfile, join
 from os import listdir
 import fnmatch
 import numpy as np
+from sklearn import svm
 
 
 # Returns first row of the csv file
 def get_csv_row_from_file(file_path):
     with open(file_path, newline='') as csvfile:
         data = csv.DictReader(csvfile)
+        items = []
+        values = []
         for row in data:
-            return row
+            while row:
+                items.append(row.popitem(last=False))
+        for item in items:
+            values.append(item[1])
+        return values
 
 
 def get_labels(file_path='./fer2013.csv'):
@@ -22,27 +29,36 @@ def get_labels(file_path='./fer2013.csv'):
     return labels
 
 
-def get_training_data():
-    file_names = retrieve_all_csvs_in_directory()
-    file_names.sort(key=lambda f: int(filter(str.isdigit, f)))
+def get_training_data(dir_path):
+    valid_files = []
+    file_names = retrieve_all_csvs_in_directory(dir_path)
+    file_names.sort(key=lambda f: int(f.split('/')[-1].split('.')[0]))
+    valid_files = [int(f.split('/')[-1].split('.')[0]) for f in file_names]
     training_data = []
-    for file_name in file_names:
-        row = np.array([int(pixel) for pixel in
-                        get_csv_row_from_file(file_name)['pixels'].split(' ')])
+    for i in range(len(file_names)):
+        row = get_csv_row_from_file(file_names[i])
         training_data.append(row)
+    
+    print(len(training_data))
+    print(valid_files)
 
-    return training_data
+    return valid_files, training_data
 
 
 def retrieve_all_csvs_in_directory(dir_path='./resized/'):
-    file_names = [[(dir_path + f) for f in listdir(dir_path) if
+    file_names = [(dir_path + f) for f in listdir(dir_path) if
                   (isfile(join(dir_path, f)) and fnmatch.fnmatch(
-                      join(dir_path, f), '*.csv'))]]
+                      join(dir_path, f), '*.csv'))]
     return file_names
 
 
 def main():
-    X = get_training_data()
-    Y = get_labels()
-
+    # get training data and train the classifier
+    X = get_training_data(
+        '/Users/timkanake/Documents/githubProj/OpenFace/build/bin/processed/')
+    # Y = get_labels()
     # clf = svm.SVC()
+
+
+if __name__ == "__main__":
+    main()
